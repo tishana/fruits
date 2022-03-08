a7a2e6f17495ce0bb7826adbe2328e2bd7201fb5
Tishana <tishana.trainor@gmail.com>
2022-03-08 15:25:15 -0500

    Please sweet babay Jesus

--- views/Show.jsx
+++ views/Show.jsx
@@ -4,7 +4,7 @@ class Show extends React.Component {
     return (
       <div>
       <h1>Fruits show page</h1>
-      The { this.props.fruit.name } is { this.props.fruit.color }
+      The { this.props.fruit.name } is { this.props.fruit.color } 
 { this.props.fruit.readyToEat ? `It is ready to eat` : `It is not ready to eat` }
   </div>
       );
